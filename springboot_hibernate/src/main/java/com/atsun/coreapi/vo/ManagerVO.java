374833b8cec348f73dd1be5ac608607a84a4264f
sunhao <784578031@qq.com>
2021-10-22 16:34:45 +0800

    第一次提交

---
+++ springboot_hibernate/src/main/java/com/atsun/coreapi/vo/ManagerVO.java
@@ -0,0 +1,12 @@
+package com.atsun.coreapi.vo;
+
+import lombok.Data;
+
+/**
+ * @author SH
+ */
+@Data
+public class ManagerVO {
+    private String username;
+    private String password;
+}
--- springboot_hibernate/src/test/java/com/atsun/coreapi/ApplicationTests.java
+++ springboot_hibernate/src/test/java/com/atsun/coreapi/ApplicationTests.java
@@ -1,13 +1,26 @@
 package com.atsun.coreapi;
 
+import com.atsun.coreapi.dao.ComplexDao;
+import com.atsun.coreapi.po.Manager;
+import com.atsun.coreapi.vo.ManagerVO;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashMap;
 
 @SpringBootTest
 class ApplicationTests {
-
+    @Qualifier("complexDaoImpl")
+    @Autowired
+    ComplexDao<Manager,String> complexDao;
     @Test
     void contextLoads() {
+        String sql="select * from t_manager tm where tm.username=?";
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("username","sunhao");
+        ManagerVO singleResultBySql = complexDao.getSingleResultBySql(sql, params, ManagerVO.class);
     }
 
 }
