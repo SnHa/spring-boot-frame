cd81dd4774b56a00a847d475b3a1f49f30bf04b6
LD <991988207@qq.com>
2021-10-27 19:01:00 +0800

    update

--- springboot_hibernate/src/main/java/com/atsun/coreapi/config/ShiroConfig.java
+++ springboot_hibernate/src/main/java/com/atsun/coreapi/config/ShiroConfig.java
@@ -1,8 +1,12 @@
 package com.atsun.coreapi.config;
 
+import com.atsun.coreapi.dto.JwtToken;
 import com.atsun.coreapi.service.ManagerService;
 import com.atsun.coreapi.vo.ManagerVO;
-import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.realm.Realm;
@@ -19,12 +23,12 @@ import java.util.HashMap;
  * @author SH
  */
 @Configuration
-public class ShiroConfig  {
+public class ShiroConfig {
 
     @Autowired
     private ManagerService managerService;
 
-    public class MyRealm extends AuthorizingRealm {
+    public class AuthRealm extends AuthorizingRealm {
 
         @Override
         protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
@@ -38,42 +42,48 @@ public class ShiroConfig  {
         protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
             // 认证
             // 获取当前用户
-            UsernamePasswordToken userToken = (UsernamePasswordToken) authenticationToken;
+            JwtToken token = (JwtToken) authenticationToken;
+
+            //TODO: 解析token，获取真实账号
+
             // 根据当前用户名对数据库用户进行比对
-            ManagerVO manager = managerService.getUser(userToken.getUsername());
+            ManagerVO manager = managerService.getUser(token.getUsername());
+
             if (manager == null) {
                 //没有该用户
                 return null;
             }
+
             // 完成认证
-            return new SimpleAuthenticationInfo(manager, manager.getPassword(), "MyRealm");
+            return new SimpleAuthenticationInfo(manager, manager.getPassword(), "AuthRealm");
         }
 
     }
 
     @Bean
-    public Realm getRealm(){
-        // Realm系统资源
-        return new MyRealm();
+    public Realm realm() {
+        return new AuthRealm();
     }
 
     @Bean
-    public DefaultWebSecurityManager getSecurityManager(Realm getRealm){
+    public DefaultWebSecurityManager getSecurityManager(Realm getRealm) {
         //securityManager  控制流程
         DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
         securityManager.setRealm(getRealm);
         return securityManager;
     }
+
     @Bean
-    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager getSecurityManager){
+    public ShiroFilterFactoryBean shiroFilterFactoryBean(DefaultWebSecurityManager getSecurityManager) {
         // ShrioFilterFactroy 拦截控制
         HashMap<String, String> filterMap = new HashMap<>();
         //配置路径过滤器 anthc表示需要登录后才能进入
-        filterMap.put("/info/**","authc");
-        filterMap.put("/menu/**","authc");
+        filterMap.put("/info/**", "authc");
+        filterMap.put("/menu/**", "authc");
         ShiroFilterFactoryBean factoryBean = new ShiroFilterFactoryBean();
         factoryBean.setSecurityManager(getSecurityManager);
         factoryBean.setFilterChainDefinitionMap(filterMap);
         return factoryBean;
     }
+
 }
--- springboot_hibernate/src/main/java/com/atsun/coreapi/controller/LoginController.java
+++ springboot_hibernate/src/main/java/com/atsun/coreapi/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.atsun.coreapi.controller;
 
+import com.atsun.coreapi.bean.DataResponse;
+import com.atsun.coreapi.bean.NoDataResponse;
+import com.atsun.coreapi.controller.data.AccountLoginData;
 import com.atsun.coreapi.dto.LoginDTO;
+import com.atsun.coreapi.enums.TransCode;
+import com.atsun.coreapi.exception.TransException;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
@@ -18,52 +23,56 @@ import static com.atsun.coreapi.enums.TransCode.ACCOUNT_BAD_CREDENTIALS;
 @Api("用户登录")
 @RestController
 @RequestMapping("/admin")
-public class LoginController {
+public class LoginController extends BaseController {
 
     @ApiOperation("登录")
-    @PostMapping("login")
-    public Object login(@RequestParam("username") String username,
-                        @RequestParam("password") String password){
-        LoginDTO loginDTO =new LoginDTO();
+    @PostMapping("/login")
+    public DataResponse<AccountLoginData> login(@RequestParam("username") String username,
+                                                @RequestParam("password") String password
+    ) throws TransException {
+        LoginDTO loginDTO = new LoginDTO();
+
         loginDTO.setUsername(username);
         loginDTO.setPassword(password);
+
         // 获取用户数据
         Subject currentUser = SecurityUtils.getSubject();
+
         // 判断是否完成认证
-        if (!currentUser.isAuthenticated()){
+        if (!currentUser.isAuthenticated()) {
             UsernamePasswordToken token = new UsernamePasswordToken(loginDTO.getUsername(), loginDTO.getPassword());
             try {
                 currentUser.login(token);
-                currentUser.getSession().setAttribute("currentUser",currentUser.getPrincipal());
-                return new BaseController().ok(currentUser.getSession().getAttribute("currentUser")) ;
+
+                //TODO: 生成jwt Token
+
+                return ok(new AccountLoginData(null));
             } catch (UnknownAccountException uae) {
                 log.info("未知账户");
-                return "未知账户";
+                throw new TransException(TransCode.CUSTOM_EXCEPTION_MSG, "未知账户");
             } catch (IncorrectCredentialsException ice) {
                 log.info("密码不正确");
-                return new BaseController().error(ACCOUNT_BAD_CREDENTIALS);
             } catch (LockedAccountException lae) {
                 log.info("账户已锁定");
-                return "账户已锁定";
             } catch (ExcessiveAttemptsException eae) {
                 return "用户名或密码错误次数过多";
             } catch (AuthenticationException ae) {
                 System.out.println("用户名或密码不正确");
                 log.info("用户名或密码不正确");
-                return new BaseController().error(ACCOUNT_BAD_CREDENTIALS);
+                return error(ACCOUNT_BAD_CREDENTIALS);
             }
-
-        }else {
-            return new BaseController().error(ACCOUNT_BAD_CREDENTIALS);
+        } else {
+            return error(ACCOUNT_BAD_CREDENTIALS);
         }
 
     }
 
     @ApiOperation("登出")
-    @DeleteMapping("out")
-    public Object out(){
+    @DeleteMapping("/logout")
+    public NoDataResponse logout() {
         Subject subject = SecurityUtils.getSubject();
         subject.logout();
-        return new BaseController().ok();
+        return ok();
     }
+
 }
